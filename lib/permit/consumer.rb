27344f7c5dfe5ef5354528d4e4c011e4393879d7
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-09-25 19:14:33 -0300

    Modifying the consumer to create and remove rules

--- lib/permit/consumer.rb
+++ lib/permit/consumer.rb
@@ -2,10 +2,12 @@ module Permit
   class Consumer
     def call(metadata, message)
       payload = message[:payload]
-      coll = Connection.pool.collection('rules')
-      policy = Policy.new(:resource_id => payload.delete(:resource_id),
-                          :collection => coll)
-      policy.create(payload)
+      if %w(create remove).include? message[:name]
+        coll = Connection.pool.collection('rules')
+        policy = Policy.new(:resource_id => payload.delete(:resource_id),
+                            :collection => coll)
+        policy.send(message[:name], payload)
+      end
     end
   end
 end
