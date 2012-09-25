f300513ae0e92a9490d0d9d224e6cd47dbacd401
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-09-25 19:14:57 -0300

    The REST API now accepts GET requests

--- lib/permit/api.rb
+++ lib/permit/api.rb
@@ -28,6 +28,29 @@ module Permit
           status 404
         end
       end
+
+      get '/' do
+        env.logger.info params
+        subject_id = params.delete(:subject_id)
+        filter = {}
+        filter[:resource_id] = params[:resource_id]
+        if params[:action]
+          filter[:actions] = {}
+          filter[:actions][params[:action]] = true
+        end
+
+        policy = Policy.new(:subject_id => subject_id, :logger => env.logger,
+                            :collection => Connection.pool.collection('rules'))
+        rules = policy.rules(filter)
+
+        if rules.count > 0
+          status 200
+          rules
+        else
+          status 404
+          rules
+        end
+      end
     end
   end
 end
