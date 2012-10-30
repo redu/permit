f12cc18d52ce235ee956511bda21f78cf6dd0ab4
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-10-30 15:11:30 -0300

    Try to make log dir on every environment

--- config/server.rb
+++ config/server.rb
@@ -3,11 +3,9 @@ require 'em-synchrony/em-mongo'
 $stdout.sync = true
 
 Permit.configure do |c|
-  if Goliath.env?(:devel) || Goliath.env?(:test)
-    dirname = "#{File.dirname(__FILE__)}/logs"
-    Dir.mkdir dirname unless File.exists?(dirname)
-    c.logger = Logger.new("#{dirname}/#{Goliath.env}.log")
-  end
+  dirname = "#{File.dirname(__FILE__)}/log"
+  Dir.mkdir dirname unless File.exists?(dirname)
+  c.logger = Logger.new("#{dirname}/#{Goliath.env}.log")
 end
 
 Permit::Connection.establish_connections
