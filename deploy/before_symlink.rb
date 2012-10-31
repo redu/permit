06061c5fb224871fba40e64b4389d46ea2f3c2ef
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-10-31 09:15:14 -0300

    Setting up example domain name and chaning location of config. example files

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,4 +1,4 @@
 run "echo 'Setting up Monit for Goliath application'"
 sudo "cp #{release_path}/deploy/scripts/#{app}_app_wrapper.sh /etc/monit.d/"
 sudo "chmod +x /etc/monit.d/#{app}_app_wrapper.sh"
-sudo "cp #{release_path}/deploy/scripts/#{app}.monitrc /etc/monit.d/"
+sudo "cp #{release_path}/config/monit/#{app}.monitrc /etc/monit.d/"
