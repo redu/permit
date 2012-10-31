d2f2268a46fde67e6dc114c65bc42459cb92ca11
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-10-31 09:06:33 -0300

    Restarting permitd when deploying

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,2 +1,8 @@
+# Restarting Goliath server
 sudo "/usr/bin/monit restart #{app}"
+
+# Reconfiguring nginx
 run "cp /data/#{app}/current/config/nginx/#{app}.conf /etc/nginx/servers"
+
+# Restarting Permit worker
+run "cd #{current_path}; /usr/bin/bundle exec permitd.rb restart"
